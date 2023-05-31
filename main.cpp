7d58b9a337ec25794b88a2b7258b5ff2f03ced7f
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-05-31 23:15:25 +0700

    Remove ground.cpp and .h, replace by use Platform.cpp to draw ground

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@ HOW TO INSTALL Microsoft.DXSDK.D3DX
 #include "AssetIDs.h"
 
 #define WINDOW_CLASS_NAME L"SampleWindow"
-#define MAIN_WINDOW_TITLE L"04 - Collision"
+#define MAIN_WINDOW_TITLE L"Super Mario Bros 3"
 #define WINDOW_ICON_PATH L"mario.ico"
 
 #define BACKGROUND_COLOR D3DXCOLOR(156.0f/255, 252.0f/255, 240.0f/255, 0.0f)

b1286ede8e4f159154bfe4788ef7431f232918b7
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-06-01 20:18:02 +0700

    Add background can draw mutiply sprite with one line in scene

--- Background.cpp
+++ Background.cpp
@@ -4,6 +4,17 @@ void CBackground::Render()
 {
 	CSprites* s = CSprites::GetInstance();
 	s->Get(this->spriteIdBackground)->Draw(x, y);
+
+	// Draw only 1 object
+	if (this->length == 1) return;
+
+	int xx = x + this->cellWidth;
+
+	for (int i = 1; i < this->length; i++)
+	{
+		s->Get(this->spriteIdBackground)->Draw(xx, y);
+		xx += this->cellWidth;
+	}
 }
 
 
